90270ebd27b5a074f8734f49b703b9b708e650f6
Saul Vega <saulaxios@gmail.com>
2017-06-05 11:22:04 -0500

    Implementando el funcionamiento del login

--- js/firebase.js
+++ js/firebase.js
@@ -1,44 +1,20 @@
-  function getUser() {
-    firebase.auth().onAuthStateChanged(function (user) {
-      if(user) {
-        console.log("Usuario logeado");
-        var user = firebase.auth().currentUser;
-        var uid = user.uid;
-        console.log(uid);
-        return uid;
-      }
-    })
-  }
-
   var db = firebase.database();
-  var privilegio;
+  var tipousuario;
 
-  var login = function() {
+  function login() {
     var email = $('#email').val();
     var contrasena = $('#contrasena').val();
-    var puesto = $('#puesto').val();
+    tipousuario = $('#puesto').val();
 
     firebase.auth().signInWithEmailAndPassword(email, contrasena)
     .catch(function(error) {
       console.log(error);
     });
 
-    let uid = getUser();
-    firebase.database().ref('usuarios/' + uid).on('value', function(snap) {
-      let usuario = snap.val();
-      privilegio = usuario.puesto;
-      console.log(privilegio);
-      console.log(puesto);
-
-      if(puesto == privilegio) {
-        console.log("hola");
-
-        $(location).attr("href", "user.html");
-      }
-    })
+    getUser();
   }
 
-  function haySesion(){
+  function getUser() {
     firebase.auth().onAuthStateChanged(function(user) {
       if (user) {
         console.log("Usuario logeado");
@@ -48,13 +24,16 @@
 
         db.ref('usuarios/' + uid).on('value', function(snap) {
           let usuario = snap.val();
-          privilegio = usuario.puesto;
+          var privilegio = usuario.puesto;
           console.log(privilegio);
 
-          if(puesto == privilegio) {
-            console.log("hola");
+          if(tipousuario == privilegio) {
+            if(privilegio == 'Administrador') {
 
-            $(location).attr("href", "user.html");
+            }
+            if(privilegio == 'Usuario') {
+                $(location).attr("href", "user.html");
+            }
           }
         })
 
@@ -62,6 +41,6 @@
     });
   }
 
-  var cerrarSesion = function() {
+  function logOut() {
     firebase.auth().signOut();
   }
