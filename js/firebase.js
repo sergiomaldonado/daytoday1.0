c2ae9917c410eb7012294920a439f7932cae379d
Saul Vega <saulaxios@gmail.com>
2017-06-06 09:31:53 -0500

    Probando los privilegios de los usuarios

--- js/firebase.js
+++ js/firebase.js
@@ -14,8 +14,8 @@
     getUser();
   }
 
-  function getUser() {
-    firebase.auth().onAuthStateChanged(function(user) {
+  function haySesion() {
+    firebase.auth().onAuthStateChanged(function (user) {
       if (user) {
         console.log("Usuario logeado");
         var user = firebase.auth().currentUser;
@@ -25,7 +25,35 @@
         db.ref('usuarios/' + uid).on('value', function(snap) {
           let usuario = snap.val();
           var privilegio = usuario.puesto;
-          console.log(privilegio);
+
+          if(privilegio == 'Administrador') {
+            $(location).attr("href", "admin.html");
+          }
+          if(privilegio == 'Usuario') {
+            $(location).attr("href", "user.html");
+          }
+        })
+      }
+      else {
+        //$(location).attr("href", "404.html");
+      }
+    })
+  }
+
+  haySesion();
+
+  function getUser() {
+    firebase.auth().onAuthStateChanged(function (user) {
+      if (user) {
+        console.log("Usuario logeado");
+        var user = firebase.auth().currentUser;
+        var uid = user.uid;
+        //console.log(uid);
+
+        db.ref('usuarios/' + uid).on('value', function(snap) {
+          let usuario = snap.val();
+          var privilegio = usuario.puesto;
+          //console.log(privilegio);
 
           if(tipousuario == privilegio) {
             if(privilegio == 'Administrador') {
