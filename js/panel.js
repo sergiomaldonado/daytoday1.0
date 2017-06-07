715051da7bcf6146e455e6bfaa67d15f4566aff4
Saul Vega <saulaxios@gmail.com>
2017-06-07 14:38:04 -0500

    Agregadas algunas animaciones a los errores de autenticación

--- js/panel.js
+++ js/panel.js
@@ -20,6 +20,9 @@ function checar() {
       $('#panel-admin').hide();
       $('#panel-usuario').hide();
       $("#modal").modal();
+      $('#email').val("");
+      $('#contrasena').val("");
+      $('#puesto').val("");
     }
   })
 }
@@ -67,6 +70,9 @@ function mostrarPanelDesdeModal() {
     }
     else {
       $('#error').html('Tu puesto es incorrecto').show();
+      setTimeout(function() {
+        $('#error').fadeOut("slow").hide();
+      }, 2000);
     }
   }, 2000);
 }
@@ -76,20 +82,34 @@ function login(){
   let password = $('#contrasena').val();
   puesto = $('#puesto').val();
 
-  firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function() {
-    mostrarPanelDesdeModal();
-  })
-  .catch(function(error) {
-    console.log(error);
+  if(email.length != 0 && password.length != 0 && puesto.length != 0) {
+    firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(function() {
+      mostrarPanelDesdeModal();
+    })
+    .catch(function(error) {
+      console.log(error);
 
-    if(error.code === 'auth/user-not-found') {
-      $('#error').html('El usuario es incorrecto').show();
-    }
-    if(error.code === 'auth/wrong-password') {
-      $('#error').html('La contraseña es incorrecta').show();
-    }
-  });
+      if(error.code === 'auth/user-not-found') {
+        $('#error').html('El usuario es incorrecto').fadeIn("slow").show();
+        setTimeout(function() {
+          $('#error').fadeOut("slow").hide();
+        }, 2000);
+      }
+      if(error.code === 'auth/wrong-password') {
+        $('#error').html('La contraseña es incorrecta').fadeIn("slow").show();
+        setTimeout(function() {
+          $('#error').fadeOut("slow").hide();
+        }, 2000);
+      }
+    });
+  }
+  else {
+    $('#error').html("Rellena todos los campos").fadeIn("slow").show();
+    setTimeout(function() {
+      $('#error').fadeOut("slow").hide();
+    }, 2000);
+  }
 }
 
 function mostrarPanel() {
